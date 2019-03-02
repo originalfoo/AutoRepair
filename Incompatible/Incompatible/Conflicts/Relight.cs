87bc29d303a576efd70fe3d429081901ed8f77d5
Guy Fraser <guy.fraser1@gmail.com>
2019-03-02 18:46:16 +0000

    Update Relight.cs
    Sketchingn out ideas for how a conflict script will work...

--- Incompatible/Incompatible/Conflicts/Relight.cs
+++ Incompatible/Incompatible/Conflicts/Relight.cs
@@ -6,6 +6,26 @@
 
     public static class Relight
     {
-        // todo
+        // if this mod is installed...
+        static readonly ulong mod = 1209581656;
+
+        // ...run this method
+        static void Verify(bool enabled)
+        {
+            if (!enabled)
+            {
+                return;
+            }
+
+
+            // Disable 'Softer Shadows' mod (643364914)
+
+            // Change settings in 'Daylight Classic' mod (530871278)
+            // 1. Disable 'Classic sunlight color' option
+            // 2. Disable 'Classic sunlight intensity' option
+            // 3. Disable 'Classic fog color for AD' option
+
+            // i have no idea how to do this!
+        }
     }
 }
